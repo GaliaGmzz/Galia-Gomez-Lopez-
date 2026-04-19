43933459050fdf0cb4d9fe6c5a30537066223285
GaliaGmzz <galiagomez2007@gmail.com>
2026-04-18 22:55:05 -0600

    Actualizado

--- Unidad II/main.py
+++ Unidad II/main.py
@@ -1,14 +1,40 @@
 from Modelos.libro import Libro
-from Modelos.usuario import Usuario
+from Modelos.biblioteca import Biblioteca
+from Modelos.bibliotecario import Bibliotecario
+from Modelos.alumno import Alumno
 from Servicios.gestorprestamos import GestorPrestamos
+from Modelos.usuario import Usuario
 
-libro1= Libro("Pepa pig","Bucanero","12345")
-usuario1= Usuario("Valeria","2321578")
+#Crear biblioteca
+biblioteca = Biblioteca ("Biblioteca Central")
 
-gestor= GestorPrestamos()
+#Crear bibliotecario
+bibliotecario1 = Bibliotecario ("Carlos", "B001", "IA") 
 
-mensaje= gestor.realizar_prestamo(libro1,usuario1,"2026-03-07")
-print(mensaje)
-print(libro1.disponibilidad)
+#Crear libros
+libro1 = Libro("Pepa Pig", "Bucanero", "12345")
+libro2 = Libro("Teoría de Evolución del Ser Humano", "Jorge el curioso", "12345")
+
+#Bibliotecario registra libros
+bibliotecario1.registrar_libro(biblioteca, libro1)
+bibliotecario1.registrar_libro(biblioteca, libro2)
+
+#Libros disponibles
+biblioteca.listar_libros()
+
+#Crear usuario
+alumno = Alumno("Galia", "2210431", "Alumno")
+
+#Crear gestor de prestamos
+gestor = GestorPrestamos()
+
+#Realizar prestamo
+print(gestor.realizar_prestamo(libro1, alumno, "18-04-2026"))
+
+#Imprimir prestamos
 gestor.listar_prestamos()
+
+print(bibliotecario1.descripcion())
+print(alumno.descripcion())
+
 
